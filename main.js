f07efe08f9e282f9b7778f416498df823fe38b6a
Aakash <Akashgautam10071997@gmail.com>
2017-07-11 15:45:48 +0530

    currently playing song name, album and images in app changes by changing song

--- main.js
+++ main.js
@@ -1,4 +1,51 @@
- //------------------------------- Functions --------------------------------------
+
+     var songs = [{
+        'name': 'Badri Ki Dulhania (Title Track)',
+        'artist': 'Neha Kakkar, Monali Thakur, Ikka Singh, Dev Negi',
+        'album': 'Badrinath ki Dulhania',
+        'duration': '2:56',
+       'fileName': 'song1.mp3',
+         'image': 'song1.jpg'
+    },
+    {
+        'name': 'Humma Song',
+        'artist': 'Badshah, Jubin Nautiyal, Shashaa Tirupati',
+        'album': 'Ok Jaanu',
+        'duration': '3:15',
+        'fileName': 'song2.mp3',
+         'image': 'song2.jpg'
+    },
+    {
+        'name': 'Nashe Si Chadh Gayi',
+        'artist': 'Arijit Singh',
+        'album': 'Befikre',
+        'duration': '2:34',
+        'fileName': 'song3.mp3',
+         'image': 'song3.jpg'
+    },
+    {
+        'name': 'The Breakup Song',
+        'artist': 'Nakash Aziz, Arijit Singh, Badshah, Jonita Gandhi',
+        'album': 'Ae Dil Hai Mushkil',
+        'duration': '2:29',
+        'fileName': 'song4.mp3',
+         'image': 'song4.jpg'
+    }]
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------- Functions --------------------------------------
    
     function toggleSong() {                                       // Here a func. is created called togglesong
         var song = document.querySelector('audio');
@@ -49,29 +96,68 @@
         $(".time-elapsed").text(currentTime);
         $(".song-duration").text(duration);
     }
+
+  // ------------------------------ change current song details func. --------------
+
+   function changeCurrentSongDetails(songs) {
+    $('.current-song-image').attr('src','img/' + songs.image);
+    $('.current-song-name').text(songs.name);
+    $('.current-song-album').text(songs.album);
+   }
+
+       
+ // --------------------------------------------------------------------------------
+
     
  // -------------------------- addSongNameClickEvent func. -------------------------  
     
-    function addSongNameClickEvent(songName, position){
+//    function addSongNameClickEvent(songName, position){
+//        var id = '#song'+ position;
+//        $(id).click(function() {
+//        var audio = document.querySelector('audio');
+//        var currentSong = audio.src;
+//        if(currentSong.search(songName) != -1)              // it is searching song name in audio src
+//        {
+//        toggleSong();
+//        }
+//        else {
+//        audio.src = songName ;
+//        toggleSong();
+//        }
+//        });
+//      }}
+    
+
+
+    function addSongNameClickEvent(songObj,position) {
+   // var songName = songs.fileName; // New Variable
+         var songName = songObj.fileName; // New Variable 
         var id = '#song'+ position;
-        $(id).click(function() {
-        var audio = document.querySelector('audio');
+      $(id).click(function() {
+         var audio = document.querySelector('audio');
         var currentSong = audio.src;
-        if(currentSong.search(songName) != -1)              // it is searching song name in audio src
+          if(currentSong.search(songName) != -1)              // it is searching song name in audio src
         {
         toggleSong();
+        
         }
         else {
-        audio.src = songName ;
-        toggleSong();
-        }
-        });
-      }
-    
+            audio.src = songName;
+            toggleSong();
+            changeCurrentSongDetails(songObj); // Function Call
+        
+       }
+      });
+    };
+
     
     
     
  //--------------------------------------------------------------------------------
+
+
+
+
    
     $('.welcome-screen button').on('click', function() {
     var name = $('#name-input').val();           // name stores the value from input
@@ -121,40 +207,12 @@
 
 
 
-     var songs = [{
-        'name': 'Badri Ki Dulhania (Title Track)',
-        'artist': 'Neha Kakkar, Monali Thakur, Ikka Singh, Dev Negi',
-        'album': 'Badrinath ki Dulhania',
-        'duration': '2:56',
-       'fileName': 'song1.mp3'
-    },
-    {
-        'name': 'Humma Song',
-        'artist': 'Badshah, Jubin Nautiyal, Shashaa Tirupati',
-        'album': 'Ok Jaanu',
-        'duration': '3:15',
-        'fileName': 'song2.mp3'
-    },
-    {
-        'name': 'Nashe Si Chadh Gayi',
-        'artist': 'Arijit Singh',
-        'album': 'Befikre',
-        'duration': '2:34',
-        'fileName': 'song3.mp3'
-    },
-    {
-        'name': 'The Breakup Song',
-        'artist': 'Nakash Aziz, Arijit Singh, Badshah, Jonita Gandhi',
-        'album': 'Ae Dil Hai Mushkil',
-        'duration': '2:29',
-        'fileName': 'song4.mp3'
-    }]
- 
    window.onload = function()
    {  
+         
 //       for(var i =0; i < songList.length;i++) {
 //        var name = '#song' + (i+1);
-//        var song = $(name);
+//        var song = $(name);x
 //        song.find('.song-name').text(songList[i]);
 //        song.find('.song-artist').text(artistList[i]);
 //        song.find('.song-album').text(albumList[i]); // Added
@@ -169,7 +227,7 @@
         song.find('.song-artist').text(obj.artist);               
         song.find('.song-album').text(obj.album);
         song.find('.song-length').text(obj.duration);
-        addSongNameClickEvent(obj.fileName,i+1);
+        addSongNameClickEvent(obj,i+1);
     }
        
       updateCurrentTime();
